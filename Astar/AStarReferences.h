7ea80ce047df2c412d6e233fbc893dd8061079f3
DESKTOP-JAGH2QT\DOMINIONXVII <dominic.b.gendron@gmail.com>
2018-11-13 23:49:53 -0500

    first bugged commit

---
+++ Astar/AStarReferences.h
@@ -0,0 +1,59 @@
+#include "pch.h"
+#include <vector>
+#pragma once
+
+#ifndef ASTAR_REF_H
+#define ASTAR_REF_H
+
+class Operation
+{
+public:
+	float cost;
+
+	Operation(float cost) :cost(cost) {}
+	~Operation() {}
+};
+
+class State
+{
+public:
+	State *parent;
+	Operation *actionFromParent;
+	double f, g, h;
+	virtual int compareTo(State *state) = 0;
+
+	State() {}
+	~State() {}
+};
+
+class Goal
+{
+public:
+	virtual bool isGoalSatisfied(State *state) = 0;
+
+	Goal() {}
+	~Goal() {}
+};
+
+
+class Heuristic
+{
+public:
+	virtual double estimateCost(State *state) = 0;
+	Heuristic(Goal *goal) : goal(goal) {}
+	~Heuristic() {}
+private:
+	Goal *goal;
+};
+
+class World
+{
+public:
+	virtual std::vector<Operation*> getActions(State *state) = 0;
+	virtual State* execute(State *state, Operation *operation) = 0;
+
+	World() {}
+	~World() {}
+};
+
+#endif
